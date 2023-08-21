f6c841b0402adcf95cc0f276006bcae08537538e
claude-at-183571538319 <claude.ducharme+lafleet-poc@gmail.com>
2023-08-22 02:23:01 +0800

    Set default values back for the project

--- set_project_values.sh
+++ set_project_values.sh
@@ -2,13 +2,13 @@
 
 # See README.md for valid values
 
-#export PROJECT_NAME=hello
+#export PROJECT_NAME=bonjour
 #export DOCKERFILE=Dockerfile_aws
 
 #export PROJECT_NAME=salut
 #export DOCKERFILE=Dockerfile_bookworm
 
-export PROJECT_NAME=bonjour
+export PROJECT_NAME=hello
 export DOCKERFILE=Dockerfile_ubuntu
 
 #export PROJECT_NAME=coucou
